e3e74a3f2a15cd0eccecdc6a3bedde21b6d07f2d
你的名字 <1811242875qq.com>
2025-09-08 14:40:47 +0800

    two

--- app/test/testcase/test_login.py
+++ app/test/testcase/test_login.py
@@ -1,4 +1,7 @@
 import csv
+
+import allure
+
 from app.schemas.ums_admin_login_param import UmsAdminLoginParam
 from app.api.ums_admin_api import UmsAdminApi
 import pytest
@@ -16,8 +19,11 @@ def read(path):
 class TestLogin:
     @pytest.mark.parametrize("username,password,res,res1",read("../resource/login_data.csv"))
     def test_login(self,username,password,res,res1):
-        user =UmsAdminLoginParam(username,password)
-        ass=UmsAdminApi().login(user).message
-        ass1=UmsAdminApi().login(user).result
-        assert res == ass
-        assert bool(res1) == ass1
+        with allure.step("创建用户对象"):
+            user =UmsAdminLoginParam(username,password)
+        with allure.step("调用登录方法"):
+            ass=UmsAdminApi().login(user).message
+            ass1=UmsAdminApi().login(user).result
+        with allure.step("断言"):
+            assert res == ass
+            assert bool(res1) == ass1
